2238f559bc5af85e6ba30ab48a655cf69d806e94
Hugo TODESCHINI <hugotodeschini@FRART0131M.local>
2020-09-22 22:04:39 +0200

    refactorization and addition of label encoding

--- libs/data_engineering.py
+++ libs/data_engineering.py
@@ -1,6 +1,8 @@
+import re
 import pandas as pd
+import numpy as np
 
-def calculerLag(df,lag):
+def calculateLag(df,lag):
     df_lag = pd.DataFrame()
     for column in df.columns:
         df_column = pd.DataFrame(df[column])
@@ -15,3 +17,76 @@ def calculerLag(df,lag):
         df_lag = pd.concat([df_lag, df_column], ignore_index=False)
     return(df_lag)
 
+#Found here: https://github.com/Arturus/kaggle-web-traffic/blob/master/extractor.py
+
+term_pat = re.compile('(.+?):(.+)')
+pat = re.compile(
+    '(.+)_([a-z][a-z]\.)?((?:wikipedia\.org)|(?:commons\.wikimedia\.org)|(?:www\.mediawiki\.org))_([a-z_-]+?)$')
+
+# Debug output to ensure pattern still works
+# print(pat.fullmatch('BLEACH_zh.wikipedia.org_all-accessspider').groups())
+# print(pat.fullmatch('Accueil_commons.wikimedia.org_all-access_spider').groups())
+
+def extract_url(source) -> pd.DataFrame:
+    """
+    Extracts features from url. Features: agent, site, country, term, marker
+    :param source: urls
+    :return: DataFrame, one column per feature
+    """
+    if isinstance(source, pd.Series):
+        source = source.values
+    agents = np.full_like(source, np.NaN)
+    sites = np.full_like(source, np.NaN)
+    countries = np.full_like(source, np.NaN)
+    terms = np.full_like(source, np.NaN)
+    markers = np.full_like(source, np.NaN)
+
+    for i in range(len(source)):
+        l = source[i]
+        match = pat.fullmatch(l)
+        assert match, "Non-matched string %s" % l
+        term = match.group(1)
+        country = match.group(2)
+        if country:
+            countries[i] = country[:-1]
+        site = match.group(3)
+        sites[i] = site
+        agents[i] = match.group(4)
+        if site != 'wikipedia.org':
+            term_match = term_pat.match(term)
+            if term_match:
+                markers[i] = term_match.group(1)
+                term = term_match.group(2)
+        terms[i] = term
+
+    return pd.DataFrame({
+        'agent': agents,
+        'site': sites,
+        'country': countries,
+        'term': terms,
+        'marker': markers,
+        'Page': source
+    })
+
+def prepareDataXGBoost(df,lag, encoding = 'oneHotEncoding'):
+    df = df.fillna(0)
+    df_extract = extract_url(df['Page'])
+    df = df.set_index('Page')
+    df = df.T.rename_axis('Dates')
+    df_lag = calculateLag(df,lag).reset_index()
+    df_prepared= df_lag.set_index('Page').join(df_extract.set_index('Page'))
+    df_prepared = df_prepared.reset_index().set_index(['Dates','Page']).sort_index()
+    df_prepared = df_prepared.drop(['term', 'marker'], axis = 1)
+    if  encoding == 'oneHotEncoding':
+        df_prepared = pd.get_dummies(df_prepared)
+    elif encoding == 'label':
+        print(df_prepared.columns)
+        df_prepared[['agent', 'site', 'country']] = df_prepared[['agent', 'site', 'country']].astype('category')
+        df_prepared['agent'] = df_prepared['agent'].cat.codes
+        df_prepared['site'] = df_prepared['site'].cat.codes
+        df_prepared['country'] = df_prepared['country'].cat.codes
+        #df_prepared['term'] = df_prepared['term'].cat.codes
+        #df_prepared['marker'] = df_prepared['marker'].cat.codes
+    else:
+        print("Bad encoding")
+    return df_prepared
